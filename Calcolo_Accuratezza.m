707a12fb8cf64bad970a521265d8c772ad87c2d6
Giuseppe Napolano <peppenapo@live.it>
2019-04-10 20:29:04 +0200

    Effettuati test accuratezza, lunedi facciamo test

--- Calcolo_Accuratezza.m
+++ Calcolo_Accuratezza.m
@@ -13,15 +13,17 @@ function [indice_cond,err,residuo,grafico] = Calcolo_Accuratezza(A,x,b,tipo)
     else
         error("Tipo matrice non valido");
     end
-    %% Per le matrici triangolari inf e sup mi baso sull'indice di condizionamento poichè non si può applicare Wilkinson
+    %% Per le matrici triangolari inf e sup mi baso sull'errore relativo poichè non si può applicare Wilkinson
     indice_cond = cond(A);
     x_attuale = risolve(A,b,opt);
     err = norm(x-x_attuale)/norm(x);
     %% Per le matrici piene posso applicare Gauss con pivoting parziale virtuale e calcolare il residuo con Wilkinson
     if strcmp(tipo,'full')
+        err = norm(x-x_attuale)/norm(x);
         residuo = (norm(b - A*x_attuale)/norm(b));
     end
     %% Creazione di un grafico dell'errore e del residuo
+    %se u(a)(indice di condizionamento) è piccolo allora posso stimare errore relativo e residuo perchè sono quasi uguali.
     else
         if(strcmp(tipo,'full'))
             opt.full=true;
