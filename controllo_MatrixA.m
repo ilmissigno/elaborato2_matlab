593636f314cf490ce833e24df48c482b13009ecc
Giuseppe Napolano <peppenapo@live.it>
2019-04-15 10:29:50 +0200

    jay jay okocha

--- controllo_MatrixA.m
+++ controllo_MatrixA.m
@@ -1,22 +1,26 @@
 function controllo_MatrixA(A)
 
+
+
+if(~ismatrix(A))
+       error('Err:A_isMatrix','A deve essere una matrice');
+end
+
 if(isempty(A))
-    error('Inserire una matrice non vuota')
+       error('Err:EmptyMatrix','La matrice A non deve essere vuota');
 end
 if(~isnumeric(A) || ~isreal(A))
-    error('La matrice A di ingresso deve essere una matrice di numeri reali.');
+    error('Err:Matrix_NotNumeric','La matrice A deve essere numerica');
 end
-if(~ismatrix(A))
-    error('A deve essere una matrice');
-end
+
 if(issparse(A))
-    error('A deve essere piena');
+      error('Err:Matrix_NotSparse','La matrice A nond deve essere sparsa');
 end
 if(find(~isfinite(A)))
-    error('I valori di A devono essere numeri reali finiti');
+      error('Err:Matrix_NotFinite','I valori di A devono essere numeri reali finiti');
 end
 if(size(A,1)~=size(A,2))
-    error('La matrice A di ingresso deve essere quadrata');
+     error('Err:Matrix_NotQuadratic','La matrice A di ingresso deve essere quadrata');
 end
 
 end
--- controllo_VectorB.m
+++ controllo_VectorB.m
@@ -1,19 +1,20 @@
 function controllo_VectorB(A,b)
 
-if(~isnumeric(b) || ~isreal(b))
-    error('Il vettore B deve essere un vettore di numeri reali');
+if(~isvector(b))
+   error('Err:BVector','B deve essere un vettore');
 end
 
-if(~isvector(b))
-    error('B deve essere un vettore');
+if(~isnumeric(b) || ~isreal(b))
+       error('Err:BReal','Il vettore B deve essere un vettore di numeri reali');
 end
+
 
 if(find(~isfinite(b)))
-    error('I valori di B devono essere numeri reali finiti');
+  error('Err:BRealFinito','Il vettore B deve essere un vettore di numeri reali finito');
 end
 
-if(size(A,2) ~= length(b))
-    error('Il vettore B deve avere lo stesso numero di righe di A');
+if(size(A) ~= length(b))
+     error('Err:CoerenzaAeB','Il vettore B deve avere lo stesso numero di righe di A');
 end
 
 end
