b18d9bcd547fc1818aeb8f371a9fb5b83c506fb8
Raffaele Formisano <raff.formisano@studenti.unina.it>
2019-04-16 14:49:05 +0200

    pipi

--- risolve.m
+++ risolve.m
@@ -14,23 +14,23 @@ controllo_VectorB(A,b);
 controllo_StructOpt(opt,A);
 %% Inizializzazione Variabili
 [n,x,e] = init_Var(A);
-%% Controllo Singolarita' Matrice A
-if(any(find(abs(diag(A))<e))==1)
-     error('Err:A_Singolare','A deve essere una matrice non singolare');
-end
 
 %% Inizio Algoritmo : Verifica valore di struttura opt
 if(opt.sup==true)
+  %% Controllo Singolarita' Matrice A triangolare superiore
+  if(any(find(abs(diag(A))<e))==1)
+       error('Err:A_Singolare','A deve essere una matrice non singolare');
+  end
     x = BS_Algorithm(A,b,n);
 elseif(opt.inf==true)
+  %% Controllo Singolarita' Matrice A triangolare inferiore
+  if(any(find(abs(diag(A))<e))==1)
+       error('Err:A_Singolare','A deve essere una matrice non singolare');
+  end
     x = FS_Algorithm(A,b,n);
 elseif(opt.full==true)
     x = Gauss_BS_Algorithm(A,b,n,e);
 end
 x=x'; % Inversione elementi (trasposta) vettore soluzione
- 
+
 end
-
-
-
-
