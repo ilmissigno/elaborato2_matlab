c02dabbfdd27dfd1eb0c73f3543201e901f78773
Giuseppe Napolano <peppenapo@live.it>
2019-04-16 16:48:51 +0200

    jk

--- Valuta_Performance.m
+++ Valuta_Performance.m
@@ -1,55 +1,38 @@
-function Valuta_Performance(inizio,fine,passo,tipoMatrice)
-
-warning off;
-    vettore_tempi = zeros(2,(fine-inizio)/passo);
+function  Valuta_Performance(inizio, fine, passo, tipo)
+%PERFORMANCE_CALCULATOR Summary of this function goes here
+%   Detailed explanation goes here
+    warning off;
+    tempi_esecuzione= zeros(2,(fine-inizio)/passo);
     j=1;
-    vettore_NRighe = zeros(1,(fine-inizio)/passo);
+    dim_vettore = zeros(1,(fine-inizio)/passo);
     for i=inizio:passo:fine
-        A = rand(i); b = rand(i,1);  
-        if strcmp(tipoMatrice,'inf')
+        A = rand(i); b = rand(i,1); 
+        if strcmp(tipo,'inf')
             A = tril(A);
             opt.inf=true;
             opt.sup=false;
-             opt.full=false;
-        elseif strcmp(tipoMatrice,'sup')
+            opt.full=false;
+        elseif strcmp(tipo,'sup')
             A = triu(A);
             opt.sup=true;
+            opt.full=false;
             opt.inf=false;
-             opt.full=false;
-        else if strcmp(tipoMatrice,'full')
-            A=full(A);
+        elseif strcmp(tipo,'full')
             opt.full=true;
+            opt.sup=false;
             opt.inf=false;
-             opt.sup=false;
-                   
         end
-        f = @() risolve(A,b,opt); 
-        f_mlDivide = @() A\b; %MLDivide
-        vettore_tempi(1,j) = timeit(f);
-        vettore_tempi(2,j) = timeit(f_mlDivide);
-        vettore_NRighe(j) = i;
+        f = @() risolve(A,b,opt); % handle to function
+        f_mlDivide = @() A\b;
+        tempi_esecuzione(1,j) = timeit(f);
+        tempi_esecuzione(2,j) = timeit(f_mlDivide);
+        dim_vettore(j) = i;
         j = j+1;
     end
-    plot( vettore_NRighe,vettore_tempi(1,:),'r',vettore_NRighe,vettore_tempi(2,:),'b')
-    title("Performance Risolve")
-    xlabel('Numero di righe')
-   ylabel('Tempo di esecuzione (s)')
-   legend('risolve','mldivide')
- 
+    plot( dim_vettore,tempi_esecuzione(1,:),dim_vettore,tempi_esecuzione(2,:))
+    title("Performance dell'algoritmo")
+    xlabel('Numero di righe di A')
+    ylabel('Tempo di esecuzione (s)')
+    legend('risolve','mldivide')
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
